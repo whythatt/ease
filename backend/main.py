20b71153abaa1b8017a489ef1a7a47cb793dc9f0
whythatt <zakrogo513@gmail.com>
2025-09-02 17:05:08 +0300

    Добавли эндпоинт /goods для получения данных парсера Создал схемы для товаров Изменил ответ парсера, чтобы данные возвращались в словаре

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
-# from routers import order, product, shop, user, basket
+from routers import goods
 
 
 app = FastAPI()
 
-# app.include_router(user.router)
-# app.include_router(shop.router)
-# app.include_router(product.router)
-# app.include_router(basket.router)
-# app.include_router(order.router)
+app.include_router(goods.router)
--- backend/routers/goods.py
+++ backend/routers/goods.py
@@ -1,9 +1,23 @@
 from fastapi import APIRouter
 from fastapi import APIRouter, Depends
 
+from schemas.goods import GoodsResponseSchema
+
+import sys
+from pathlib import Path
+
+# Добавляем путь к папке python в sys.path
+python_path = Path(__file__).parent.parent.parent  # /Users/niktar/python
+sys.path.insert(0, str(python_path))
+
+# Теперь импортируем абсолютным путем
+from parser.main import fetch_data
+
 
 router = APIRouter(prefix="/goods", tags=["Goods APIs"])
 
-@router.get('/')
-async def parse_goods():
-    pass
+
+@router.get("/", response_model=GoodsResponseSchema)
+def parse_goods(image_url: str) -> GoodsResponseSchema:
+    parser_data = fetch_data(image_url=image_url)
+    return parser_data
